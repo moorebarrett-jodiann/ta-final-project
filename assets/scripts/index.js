c33b240418d1ab817bb1058d712c7a0a9d22c5f4
Jodi-Ann Moore-Barrett <moorebarrett.jodiann@gmail.com>
2022-11-15 11:06:30 -0600

    adjust form validation

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -2,19 +2,27 @@
 
 const validate = document.querySelector('.submit');
 
-const email = document.querySelector('.email');
 const firstName = document.querySelector('.first-name');
 const lastName = document.querySelector('.last-name');
 const message = document.querySelector('.message');
+let email = document.querySelector('.email').value.trim();
+
+const emailRegex = /^[^\s@]+@[^\s@]+.[^\s@]+$/;
 
 validate.addEventListener('click', () =>{
     if (firstName.value == "" || lastName.value == ""){
         alert('Please add your full name')
     }
+    if(email.length === 0) {
+        alert('Please enter your email');
+    }
+    else if(!emailRegex.test(email)) {
+        alert('Please enter a valid email');
+    }
     else if(message.value == ""){
         alert('Please add your message')
     }
     else{
-        alert('Message has been sent')
+        alert('message has been sent')
     }
-})
+});
